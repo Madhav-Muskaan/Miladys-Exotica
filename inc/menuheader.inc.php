ca64591b5bb946cb6a3fdf1ff3816bb655a2ab83
Madhav-Muskaan <92105827+Madhav-Muskaan@users.noreply.github.com>
2021-10-17 10:21:45 +0530

    Add files via upload

---
+++ inc/menuheader.inc.php
@@ -0,0 +1,16 @@
+<div class="categolis">
+		<table>
+			<tr>
+				<th>
+					<a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #24bfae;border-radius: 12px;">Ethnic Wear</a>
+				</th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Jewelry</a></th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Watch</a></th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Perfume</a></th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Bracelets</a></th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Casual Wear</a></th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">FootWear</a></th>
+				<th><a href="ethnicwear.php" style="text-decoration: none;color: #ddd;padding: 4px 12px;background-color: #c7587e;border-radius: 12px;">Hair Accessories</a></th>
+			</tr>
+		</table>
+	</div>
